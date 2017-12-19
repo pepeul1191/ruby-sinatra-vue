a08ef669354ab2ae9bfd386029af949dd25d1316
Pepe Valdivia <jvaldivia@softweb.pe>
2017-12-19 15:10:56 -0500

    Router incluido

--- gulpfile.js
+++ gulpfile.js
@@ -284,6 +284,7 @@ gulp.task('vue', function(){
     gulp.start('fonts', 'layout-css', 'layout-js', 'swp-plugins');
     gulp.src([
         MEDIA + 'bower_components/vue/dist/vue.min.js',
+        MEDIA + 'bower_components/vue-router/dist/vue-router.min.js',
         MEDIA + 'bower_components/jquery/dist/jquery.min.js',
         MEDIA + 'bower_components/handlebars/handlebars.min.js',
         MEDIA + 'js/index.js'
--- public/js/index.js
+++ public/js/index.js
@@ -43,5 +43,52 @@ var app5 = new Vue({
 })
 
 Vue.component('todo-item', {
-  template: '<li>This is a todo</li>'
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
 })
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
+})
+
+
+// 0. If using a module system (e.g. via vue-cli), import Vue and VueRouter
+// and then call `Vue.use(VueRouter)`.
+
+// 1. Define route components.
+// These can be imported from other files
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+// 2. Define some routes
+// Each route should map to a component. The "component" can
+// either be an actual component constructor created via
+// `Vue.extend()`, or just a component options object.
+// We'll talk about nested routes later.
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
+
+// 4. Create and mount the root instance.
+// Make sure to inject the router with the router option to make the
+// whole app router-aware.
+const app8 = new Vue({
+  router
+}).$mount('#app-8')
+
+// Now the app has started!
